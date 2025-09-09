eb767e6c04cf9a9fe6ff8bfbe64eedb8819fba81
Felipe Mata <felipe.mata@ucr.ac.cr>
2025-09-09 02:29:40 +0200

    Agrego Lab 2

---
+++ lab_2/ejercicio_1/ejercicio1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+###Script para:
+#Ver si el usaurio que ejecuta es root.
+#Recibe párametros (usario, nombre de un grupo y ruta de un archivo)
+#MOdificar la pretencia del archivo 
+#Modificar los permisos de lectura, escritura y ejecución
+#Hacer caso si existe o no existe y psoibles errores y ayuda si no sabe ejecutar
+
+
+|
