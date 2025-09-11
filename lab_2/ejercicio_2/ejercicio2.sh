d2973cf428eb0e5f220e6540efb1d6bc9567de4c
Felipe Mata <felipe.mata@ucr.ac.cr>
2025-09-11 05:22:46 +0200

    Cambio en el script, quitar lineas extra

--- lab_2/ejercicio_2/ejercicio2.sh
+++ lab_2/ejercicio_2/ejercicio2.sh
@@ -37,7 +37,46 @@ if [ $# -ne 1 ];then
 	exit 1
 fi
 
+#Archivo
+LOGFILE="$1.log"
+echo "# Tiempo(s)   CPU(%)   MEM(%)" > "$LOGFILE"
+
+#Proceso en segundo plano
+"$1" &
+PID=$!
+
+#Monitoreo
+START=$(date +%s)
+while kill -0 $PID 2>/dev/null; do
+    NOW=$(date +%s)
+    ELAPSED=$((NOW - START))
+
+	# CPU y MEM 
+    USAGE=$(ps -p $PID -o %cpu,%mem --no-headers)
+    CPU=$(echo $USAGE | awk '{print $1}')
+    MEM=$(echo $USAGE | awk '{print $2}')
+	#guardar en logfile
+    echo "$ELAPSED   $CPU   $MEM" >> "$LOGFILE"
+
+    sleep 2  # intervalo de muestreo (2s)
+done
+
+echo " Proceso finalizado."
+echo " Datos guardados en $LOGFILE"
 
 
+#Gráfica
+#
+GNUPLOT_SCRIPT="plot.gnuplot"
+cat > $GNUPLOT_SCRIPT <<EOF
+set terminal png size 800,600
+set output 'monitor.png'
+set title "Consumo de CPU y Memoria"
+set xlabel "Tiempo (s)"
+set ylabel "Uso (%)"
+set grid
+plot "$LOGFILE" using 1:2 with lines title "CPU (%)", \
+     "$LOGFILE" using 1:3 with lines title "Mem (%)"
+EOF
 
-
+gnuplot $GNUPLOT_SCRIPT
