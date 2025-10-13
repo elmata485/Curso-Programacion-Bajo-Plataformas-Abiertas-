1d38881af0e6815457cdf63ca73ce6f92d8d623e
Felipe Mata <felipe.mata@ucr.ac.cr>
2025-10-13 04:48:38 +0200

    Bubblesort implementación

--- lab_4/ejercicio1/ejercicio1.c
+++ lab_4/ejercicio1/ejercicio1.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+
 void Imprimir_mat(int *ptr, int filas, int columnas) {
 	int i, j;
 	for (i=0; i<filas; i++){
 		for (j=0; j<columnas; j++){
-		printf("%4d", *(ptr + i * columnas +j));	
+		printf("%4d", *(ptr + i * columnas +j));// Imprimo el número que se encuentra en la posición de la matriz que le indico mediante la utilización de punteros	
 		}
 		printf("\n");	
 	}
@@ -12,7 +13,20 @@ void Imprimir_mat(int *ptr, int filas, int columnas) {
 }
 
 
-
+void Bubblesort(int *ptr, int filasxcols){
+	int i, j, temp;
+	for (i=0; i<filasxcols-1;	i++){
+		for (j=0; j<filasxcols-i-1; j++){
+			// tomar hasta el penultimo número ya que si se prueba con el último puede cambiarse con dirreciónes que tienen basura
+			if (*(ptr +j)> *(ptr +j+1)){
+				//intercambiar con la ayuda de una variable temporal
+				temp = *(ptr +j);
+				*(ptr + j) = *(ptr +j+1);
+				*(ptr +j +1) = temp;
+			}
+		}
+	}
+}
 
 
 
@@ -26,6 +40,8 @@ int main() {
 
 	printf("La matriz a reordenar es: \n");
 	Imprimir_mat((int *)mat_1, 2, 3);
-	
+	Bubblesort((int *)mat_1, 2 * 3);
+	printf("La matriz ya se ordenó, quedó de la siguiente manera: \n");
+	Imprimir_mat((int *)mat_1, 2, 3);	
 }
 
