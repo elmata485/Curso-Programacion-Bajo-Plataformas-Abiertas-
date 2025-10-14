e72f724951d604f3a30ae3510d53ceadfcbe8d44
Felipe Mata <felipe.mata@ucr.ac.cr>
2025-10-14 21:22:43 +0200

    Abrir y cerrar archivos a utilizar y cambiar

--- lab_4/ejercicio2/ejercicio2.c
+++ lab_4/ejercicio2/ejercicio2.c
@@ -4,16 +4,20 @@
 
 
 void limpiarPalabra(char *palabra) {
-    int len = strlen(palabra);
-    int inicio = 0, fin = len - 1;
+    int len = strlen(palabra); //longitud de palabra
+    int inicio = 0, fin = len - 1; 
 
-    while (inicio <= fin && !isalnum((unsigned char)palabra[inicio])) inicio++;
-    while (fin >= inicio && !isalnum((unsigned char)palabra[fin])) fin--;
+    while (inicio <= fin && !isalnum((unsigned char)palabra[inicio])) {
+        inicio++;
+    }// avanza hasta que el caracter sea alfanumerico
+    while (fin >= inicio && !isalnum((unsigned char)palabra[fin])) {
+        fin--;
+    }//retrocede el fin hasta que el caracter sea alfanumerico
 
     int j = 0;
     for (int i = inicio; i <= fin; i++)
-        palabra[j++] = palabra[i];
-    palabra[j] = '\0';
+        palabra[j++] = palabra[i];// recorta la palbra dejandola sin signos de puntuación
+    palabra[j] = '\0';//marca el fin de la palabra
 }
 
 int main(int argc, char *argv[]) {
@@ -22,5 +26,32 @@ int main(int argc, char *argv[]) {
 			<palabra_buscar> <palabra_reemplazo> \n", argv[0]);
         return 1;
     }// condición de que el usuario escriba los argumentos necesarios
-   
+
+
+
+
+
+
+  FILE *entrada = fopen(argv[1], "r");
+    if (!entrada) {
+        perror("Error al abrir el archivo de entrada");
+        return 1;
+    }
+
+    FILE *salida = fopen("output.txt", "w");
+    if (!salida) {
+        perror("Error al crear el archivo de salida");
+        fclose(entrada);
+        return 1;
+    }
+
+
+    fclose(entrada);
+    fclose(salida);
+
+    printf("Archivo modificado guardado en 'output.txt'\n");
+    return 0;
 }
+
+
+
