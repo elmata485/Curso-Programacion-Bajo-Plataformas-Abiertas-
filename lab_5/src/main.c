4660575b80c5bd8009aa31b9a81163863cac5cf5
Felipe Mata <felipe.mata@ucr.ac.cr>
2025-11-16 02:36:23 +0100

    Agregar saltos de línea en printf

--- lab_5/src/main.c
+++ lab_5/src/main.c
@@ -4,52 +4,48 @@
 int main() {
     Nodo* lista = NULL;
 	
-    printf("Lista al principio");
+    printf("Lista al principio\n");
     imprimir(lista);
 
 
-    printf("Lista antes de insertar al inicio");
+    printf("Lista antes de insertar al inicio\n");
     imprimir(lista);
     lista = insertar_inicio(lista, 10);
-    printf("Lista despues de insertar al inicio");
+    printf("Lista despues de insertar al inicio\n");
     imprimir(lista);
 
 
-    printf("Lista antes de insertar al final");
+    printf("Lista antes de insertar al final\n");
     imprimir(lista);
     lista = insertar_final(lista, 20);
-    printf("Lista despues de insertar al final");
+    printf("Lista despues de insertar al final \n");
     imprimir(lista);
 
 
-    printf("Lista antes de insertar en posición1");
+    printf("Lista antes de insertar en posición1 \n");
     imprimir(lista);
     lista = insertar_pos(lista, 15, 1);
-    printf("Lista después de insertar en posición 1");
+    printf("Lista después de insertar en posición 1 \n");
     imprimir(lista);
 
 
 
-    printf("Lista antes de buscar dato");
+    printf("Lista antes de buscar dato \n");
     imprimir(lista);
     Nodo* encontrado = buscar(lista, 15);
     if (encontrado) {
         printf("Encontrado: %d\n", encontrado->dato);
     }
-    printf("Lista despues de buscar dato");
+    printf("Lista despues de buscar dato \n");
     imprimir(lista);
 
-    printf("Lista antes de eliminar dato");
+    printf("Lista antes de eliminar dato \n");
     imprimir(lista);
     lista = eliminar(lista, 20);
-    printf("Lista despues de eliminar dato");
+    printf("Lista despues de eliminar dato \n");
     imprimir(lista);
 
-    printf("Lista antes de destruir lista de memoria dinámica");
-    imprimir(lista);
     destruir_lista(lista);
-    printf("Lista después  de destruir lista");
-    imprimir(lista);
 
     return 0;
 }
